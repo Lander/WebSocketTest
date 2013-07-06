ffcca4409399de9170d5b09b06cca33b052f8a31
Denis Shvets <mailforsab@gmail.com>
2013-07-06 16:25:39 +0300

    Added jQuery and Submit Event

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-// Main.js
+var ws = new WebSocket("ws://localhost:8081");
+
+$('.output').on({
+    submit: function(e){
+        e.preventDefault();
+        e.stopPropagation();
+        console.log(this);
+    }
+});
