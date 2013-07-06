d45b313aa9288b3f1ccc8cffd34f1e3fde736ce5
Denis Shvets <mailforsab@gmail.com>
2013-07-06 16:46:30 +0300

    Added Submit Input Value Functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 var ws = new WebSocket("ws://localhost:8081");
 
-$('.output').on({
-    submit: function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        console.log(this);
-    }
+$(function(){
+    $('#output').on({
+        submit: function(e){
+            e.preventDefault();
+            ws.send(this.message.value);
+        }
+    });
 });
